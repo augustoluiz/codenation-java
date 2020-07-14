aa4e7dde954fb909c7770a40974e247c466f60d4
augustoluiz <augustogu7@yahoo.com.br>
2020-07-14 00:52:57 -0300

    inicando o projeto spring

---
+++ 4-quarto_modulo/desafios/bdrelacional/src/main/java/br/com/quartodesafio/bdrelacional/BdrelacionalApplication.java
@@ -0,0 +1,13 @@
+package br.com.quartodesafio.bdrelacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdrelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdrelacionalApplication.class, args);
+	}
+
+}
---
+++ 4-quarto_modulo/desafios/bdrelacional/src/test/java/br/com/quartodesafio/bdrelacional/BdrelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quartodesafio.bdrelacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdrelacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
