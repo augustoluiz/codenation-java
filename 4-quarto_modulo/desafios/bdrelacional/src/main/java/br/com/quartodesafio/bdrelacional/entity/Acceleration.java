65408ef383035725d71a10c7f1565143871cfd91
augustoluiz <augustogu7@yahoo.com.br>
2020-07-14 01:22:03 -0300

    Definindo a entidade Acceleration

---
+++ 4-quarto_modulo/desafios/bdrelacional/src/main/java/br/com/quartodesafio/bdrelacional/entity/Acceleration.java
@@ -0,0 +1,59 @@
+package br.com.quartodesafio.bdrelacional.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.sql.Timestamp;
+
+@Entity
+public class Acceleration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @NotBlank
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(length = 50, nullable = false)
+    private String slug;
+
+    //challenge_id
+
+    @NotBlank
+    @Column(nullable = false)
+    private Timestamp created_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+}
---
+++ 4-quarto_modulo/desafios/bdrelacional/src/main/java/br/com/quartodesafio/bdrelacional/entity/Candidate.java
@@ -0,0 +1,10 @@
+package br.com.quartodesafio.bdrelacional.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Candidate {
+
+
+
+}
